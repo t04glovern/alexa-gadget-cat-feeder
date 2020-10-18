bc40631d28e72752b1735bde2de6c64550d700b9
Nathan Glover <nathan@glovers.id.au>
2020-10-18 18:26:59 +0800

    updated to use alternative gpio method

--- alexa-gadget-cat-feeder.py
+++ alexa-gadget-cat-feeder.py
@@ -1,31 +1,23 @@
+import RPi.GPIO as IO
 import json
 import logging
 import sys
 import threading
 import time
 
-from gpiozero import RGBLED, AngularServo
-from colorzero import Color
 from agt import AlexaGadget
+
+IO.setmode(IO.BCM)
 
 logging.basicConfig(stream=sys.stdout, level=logging.INFO)
 logger = logging.getLogger(__name__)
 logging.getLogger('agt.alexa_gadget').setLevel(logging.DEBUG)
 
-# GPIO Pins
-GPIO_LED_RED = 2
-GPIO_LED_GREEN = 3
-GPIO_LED_BLUE = 4
-GPIO_SERVO_PIN = 14
+# GPIO pins
+GPIO_SERVO_PIN = 18
 
-# Setup RGB LED
-# set active_high to False for common anode RGB LED
-# else set to True for common cathode RGB LED
-RGB_LED = RGBLED(GPIO_LED_RED, GPIO_LED_GREEN, GPIO_LED_BLUE,
-                 active_high=True, initial_value=(0, 0, 0))
-
-SERVO = AngularServo(GPIO_SERVO_PIN, initial_angle=-90,
-                     min_pulse_width=0.0005, max_pulse_width=0.002)
+IO.setup(GPIO_SERVO_PIN,IO.OUT)
+SERVO = IO.PWM(GPIO_SERVO_PIN,100)
 
 class CatFeederGadget(AlexaGadget):
     """
@@ -41,10 +33,6 @@ class CatFeederGadget(AlexaGadget):
         """
         self._reset_feeder()
 
-        # Turn on the LED
-        RGB_LED.color = Color('blue')
-        RGB_LED.on()
-
     def on_custom_catfeedergadget_cleanup(self, directive):
         """
         Handles Custom.CatFeederGadget.Cleanup directive sent from skill
@@ -55,30 +43,21 @@ class CatFeederGadget(AlexaGadget):
         """
         Handles Custom.CatFeederGadget.FeedCat directive sent from skill
         """
-
-        # Set angle of servo to 0 degrees
-        SERVO.angle = 0
-        time.sleep(1)
-        SERVO.detach()
+        SERVO.start(0)
+        SERVO.ChangeDutyCycle(30)
+        time.sleep(0.5)
 
         payload = {'feed': True}
         self.send_custom_event(
             'Custom.CatFeederGadget', 'ReportFeeder', payload)
 
     def _reset_feeder(self):
-        # Set angle of servo to -90 degrees
-        SERVO.angle = -90
-        time.sleep(1)
-        SERVO.detach()
-
-        # Turn off the LED
-        RGB_LED.color = Color('red')
-        RGB_LED.off()
+        SERVO.start(0)
+        SERVO.ChangeDutyCycle(15)
+        time.sleep(0.5)
 
 if __name__ == '__main__':
     try:
         CatFeederGadget().main()
     finally:
-        logger.debug('Cleaning up GPIO')
-        RGB_LED.close()
-        SERVO.close()
+        logger.debug('Cleaning up')
